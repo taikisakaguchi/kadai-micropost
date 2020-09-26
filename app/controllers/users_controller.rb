cf38f05b3351a134c32f289fa9d40fbd95079308
taiki.sakaguchi <taiki.sakaguchi826@gmail.com>
2020-09-26 13:30:58 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :require_user_logged_in, only: [:index, :show]
+before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
 
   
   def index
@@ -8,6 +8,8 @@ before_action :require_user_logged_in, only: [:index, :show]
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
@@ -24,6 +26,18 @@ before_action :require_user_logged_in, only: [:index, :show]
       flash.now[:danger] = 'ユーザの登録に失敗しました'
       render :new
     end
+  end
+  
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
   end
   
 private
