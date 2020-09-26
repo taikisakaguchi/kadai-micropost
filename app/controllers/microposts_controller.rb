59c33db7412085b1ca1569827f32b25109ea0aa1
taiki.sakaguchi <taiki.sakaguchi826@gmail.com>
2020-09-26 13:45:17 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました'
       redirect_to root_url
     else
-      @micropost = current_user.microposts.order(id: :desc).page(params[:page])
+      @micropost = current_user.feed_microposts.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました'
       render 'toppages/index'
     end
